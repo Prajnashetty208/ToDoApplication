b8d241600359974a0f19581c3e07372ee1388226
Prajnashetty208 <60169450+Prajnashetty208@users.noreply.github.com>
2021-09-14 21:07:09 +0530

    Hystrix changes

--- src/main/java/com/todo/restapp/RestappApplication.java
+++ src/main/java/com/todo/restapp/RestappApplication.java
@@ -2,8 +2,11 @@ package com.todo.restapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@Ena
 public class RestappApplication {
 
 	public static void main(String[] args) {
